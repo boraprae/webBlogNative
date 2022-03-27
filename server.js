cdd99359a17b3aba0808aac60e795e9fd7ecf2c8
boraprae <72434691+boraprae@users.noreply.github.com>
2022-03-28 02:17:46 +0700

    middleware and filter user

--- server.js
+++ server.js
@@ -16,11 +16,46 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //?------------ Data ---------------?
-var data =
-    [{ id: 0, title: 'First', detail: 'aaa' },
-    { id: 1, title: 'Second', detail: 'bbb' },
-    { id: 2, title: 'Third', detail: 'ccc' },
-    ];
+const user = [
+    { 'user_id': 1, 'user_username': 'admin', 'user_password': '1111', 'user_role': 1 },
+    { 'user_id': 2, 'user_username': 'user2', 'user_password': '2222', 'user_role': 2 },
+    { 'user_id': 3, 'user_username': 'user3', 'user_password': '3333', 'user_role': 2 },
+]
+
+const data = [
+    { 'id': 1, 'title': 'First', 'detail': 'aaa', 'user_id': 2 },
+    { 'id': 2, 'title': 'Second', 'detail': 'bbb', 'user_id': 2 },
+    { 'id': 3, 'title': 'Third', 'detail': 'ccc', 'user_id': 3 },
+    { 'id': 4, 'title': 'Fourth', 'detail': 'ddd', 'user_id': 3 },
+]
+
+// ==================== Middleware ==============
+function checkUser(req, res, next) {
+    let token = req.headers['authorization'] || req.headers['x-access-token'];
+    if (token == undefined || token == null) {
+        // no token
+        res.status(400).send('No token');
+    }
+    else {
+        // token found
+        if (req.headers.authorization) {
+            const tokenString = token.split(' ');
+            if (tokenString[0] == 'Bearer') {
+                token = tokenString[1];
+            }
+        }
+        jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+            if (err) {
+                res.status(400).send('Incorrect token');
+            }
+            else {
+                req.decoded = decoded;
+                // back to the service
+                next();
+            }
+        });
+    }
+}
 
 app.get("/", (req, res) => {
     const message = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
@@ -41,21 +76,53 @@ app.get('/mobile/blog', (_req, res) => {
     res.json(data);
 });
 
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+// app.post("/login", (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
 
-    console.log(req.body);
+//     console.log(req.body);
 
-    if (username == 'boraprae' && password == '1234') {
-        const payload = { user: username };
-        //Encode
-        const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1d' });
-        res.send({ url: '/blogs', token: token });
-    } else {
-        res.status(400).send("Login Failed");
-    }
+//     if (username == 'boraprae' && password == '1234') {
+//         const payload = { user: username };
+//         //Encode
+//         const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1d' });
+//         res.send({ url: '/blogs', token: token });
+//     } else {
+//         res.status(400).send("Login Failed");
+//     }
+// });
+
+app.post('/login', (req, res) => {
+    setTimeout(() => {
+        const { username, password } = req.body;
+        const result = user.filter((value) => {
+            return value.user_username == username && value.user_password == password;
+        });
+        // res.json(result);
+
+        if (result.length == 1) {
+            const payload = { user: username, user_id: result[0].user_id };
+            const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1d' });
+            res.json({ url: '/blogs', token: token });
+            // res.send('/blogs');
+        }
+        else {
+            res.status(400).send('Wrong username or password');
+        }
+    }, 3000);
 });
+
+app.get('/mobile/blogs', checkUser, (req, res) => {
+    // get userID from token
+    const result = req.decoded;
+    const userID = result.user_id;
+    // filter blog posts for this user
+    const posts = data.filter((value) => {
+        return value.user_id == userID;
+    });
+    res.json(posts);
+});
+
 
 app.post("/editData", (req, res) => {
     const id = req.body.ID;
